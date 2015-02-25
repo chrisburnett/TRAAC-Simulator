6884c1333de559e74a1139d6bae5055249b7e40b
Christopher Burnett <cburnett@cops-022690.uoa.abdn.ac.uk>
2015-02-25 17:26:59 +0000

    Raac class now takes in group assignments and works out the individual and group zone assignments for the recipient. Doesn't do anything with this yet (that's the hard part)

--- raac.rb
+++ raac.rb
@@ -1,49 +1,62 @@
 # this class models RAAC behaviour without trust (i.e. risk budgets and static intervals)
 class Raac
-  
+
   def initialize
     # superclasses can override this value, setting to 0 to disable risk budgeting
     @budget_decrement = Parameters::BUDGET_DECREMENT
     # track obligations
     @active_obligations = Hash.new { |h,k| h[k] = [] }
   end
-  
+
   # This function returns a pair of decision (true or false) and an obligation (can be "none")
-  def authorisation_decision(request, ind_policy, grp_policy)
-    # compute the risk
-    risk = compute_risk(request, ind_policy, grp_policy)
-
-    # get the mitigation strategy for the permission mentioned in the request
-    ms = Parameters::SENSITIVITY_TO_STRATEGIES[request[:sensitivity]]
-
+  def authorisation_decision(request, groups, ind_policy, grp_policy)
     obligation = :none
     decision = false
     fdomain = Parameters::REJECT_DOMAIN
-    # check for deny zone and check the user has enough budget for this owner
-    # (instant deny conditions)
-    if ind_policy[request[:recipient]][0] != :deny &&
-        request[:requester].risk_budget[request[:owner]] > 0
-      # now check to see which risk domain the computed risk falls into
-      risk_domains(request).each do |did, domain| 
-        if risk >= domain[0] && risk < domain[1] then
-          # set the obligation to whatever the mitigation strategy says for this risk domain
-          obligation = Parameters::MITIGATION_STRATEGIES[ms][did]
 
-          # if there was an obligation, decrease budget and add it to
-          # the active list for this agent
-          if obligation != :none then
-            add_obligation(request[:requester], obligation, request[:owner])
+    # first off, check the requester is even allowed to share. At the
+    # moment, this should always be true, because the simulator is
+    # only passing in agents from the share zone but just for
+    # completeness
+    if ind_policy[request[:requester].id] == :share then  
+      # compute the risk
+      risk = compute_risk(request, ind_policy, grp_policy)
+
+      # get the mitigation strategy for the permission mentioned in the request
+      ms = Parameters::SENSITIVITY_TO_STRATEGIES[request[:sensitivity]]
+
+      # get the individual and group policy zones this recipient belongs to
+      ind_zone = ind_policy[request[:recipient].id]
+      grp_zone = grp_policy[groups[request[:recipient]]]
+      binding.pry
+      # check for deny zone and check the user has enough budget for
+      # this owner (instant deny conditions)
+      if ind_zone != :deny &&
+         request[:requester].risk_budget[request[:owner]] > 0
+        # if the individual zone is explicitly defined (and not deny)
+
+        # now check to see which risk domain the computed risk falls into
+        risk_domains(request).each do |did, domain|
+          if risk >= domain[0] && risk < domain[1] then
+            # set the obligation to whatever the mitigation strategy says for this risk domain
+            obligation = Parameters::MITIGATION_STRATEGIES[ms][did]
+
+            # if there was an obligation, decrease budget and add it to
+            # the active list for this agent
+            if obligation != :none then
+              add_obligation(request[:requester], obligation, request[:owner])
+            end
+
+            # if we are in the last domain then reject the request,
+            # otherwise accept (poss. with obligation)
+            if did == Parameters::REJECT_DOMAIN then
+              decision = false
+            else decision = true
+            end
+
+            # record this just for logging in the result
+            fdomain = did
           end
-
-          # if we are in the last domain then reject the request,
-          # otherwise accept (poss. with obligation)
-          if did == Parameters::REJECT_DOMAIN then 
-            decision = false 
-          else decision = true 
-          end
-
-          # record this just for logging in the result
-          fdomain = did
         end
       end
     end
@@ -52,22 +65,22 @@ class Raac
     return {
       decision: decision,
       risk: risk,
-      obligation: obligation, 
+      obligation: obligation,
       domain: fdomain,
-      strategy: ms, 
+      strategy: ms,
       requester: request[:requester].id,
       recipient: request[:recipient].id,
-      source_zone: ind_policy[request[:requester].id][0], 
-      target_zone: ind_policy[request[:recipient].id][0],
+      source_zone: ind_policy[request[:requester].id],
+      target_zone: ind_policy[request[:recipient].id],
       req_budget: request[:requester].risk_budget[request[:owner]]
     }
   end
-  
+
   def add_obligation(requester, obligation, owner)
     @active_obligations[requester].push([obligation, owner])
     requester.risk_budget[owner] -= @budget_decrement
   end
-  
+
   def do_obligation(requester)
     # if this requester has any open obligations, do one and restore
     # budget
@@ -80,7 +93,7 @@ class Raac
   def fail_obligation(requester)
     @active_obligations[requester].pop
   end
-  
+
   private
 
   # This function is where everything happens - we take all the things
@@ -92,7 +105,7 @@ class Raac
     # simple approach - trust is 0 for everyone so risk is always just
     # the risk for the sensitivity label
     Parameters::SENSITIVITY_TO_LOSS[request[:sensitivity]]
-    
+
     # 0 because to use the actual risk makes the system more cautious
     # than our premissive mode, and since we don't punish bad
     # blocking, it will always do better.
@@ -105,5 +118,3 @@ class Raac
 
 
 end
-
-
