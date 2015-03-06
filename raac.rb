2c3d04d5b55398592d44d31aee2a560f33050602
Christopher Burnett <cburnett@cops-022690.uoa.abdn.ac.uk>
2015-03-06 16:57:29 +0000

    Refactoring and getting ready to implement the group extensions

--- raac.rb
+++ raac.rb
@@ -8,17 +8,12 @@ class Raac
     @active_obligations = Hash.new { |h,k| h[k] = [] }
   end
 
-  # This function returns a pair of decision (true or false) and an obligation (can be "none")
-  def authorisation_decision(request, groups, ind_policy, grp_policy)
-    obligation = :none
-    decision = false
-    fdomain = Parameters::REJECT_DOMAIN
-    
-    # first off, check the requester is even allowed to share. At the
+  def individual_decision
+    # check the requester is even allowed to share. At the
     # moment, this should always be true, because the simulator is
     # only passing in agents from the share zone but just for
     # completeness
-    if ind_policy[request[:requester].id] == :share then  
+    if ind_policy[request[:requester].id] == :share then
       # compute the risk
       risk = compute_risk(request, ind_policy, grp_policy)
 
@@ -26,13 +21,8 @@ class Raac
       ms = Parameters::SENSITIVITY_TO_STRATEGIES[request[:sensitivity]]
 
       # get the individual and group policy zones this recipient
-      # belongs to TODO: if a recipient can be members of multiple
-      # groups, how do we handle access checking? I think we need to
-      # check all the groups, and see if any are deny. If not (meaning
-      # all the recipient's groups are either undefined or read/share)
-      # then group access is allowable.
+      # belongs to
       ind_zone = ind_policy[request[:recipient].id]
-      grp_zone = grp_policy[groups[request[:recipient]]]
 
       # check for deny zone and check the user has enough budget for
       # this owner (instant deny conditions)
@@ -58,25 +48,38 @@ class Raac
               decision = false
             else decision = true
             end
-
-            # record this just for logging in the result
-            fdomain = did
           end
         end
       end
     end
+    return decision, obligation, did
+  end
+
+  # This function returns a pair of decision (true or false) and an obligation (can be "none")
+  def authorisation_decision(request, groups, ind_policy, grp_policy)
+    obligation = :none
+    decision = false
+    fdomain = Parameters::REJECT_DOMAIN
+
+    # first, check whether there is an explicit individual zone
+    # assignment. If not, we'll check the group zone
+    if not ind_policy[request[:requester].id] == :undefined then
+      decision, obligation, fdomain = individual_decision(request, ind_policy)
+    else
+      # otherwise we need to check whether the group is allowed to
+      # share, and work out the risk of sharing on that basis (and
+      # possibly using group risk mitigation strategies)
+    end
 
     # the recipient might be a group, in which case it's already just
     # a symbol and we don't need to call ID (like we do for
-    # individuals) TODO: to fix this, you'd really need to make groups
-    # a class that responds to 'id'. Not a tidy solution, this, but
-    # it'll do for now
+    # individuals)
     recipient = if request[:recipient].respond_to?(:id) then
                   request[:recipient].id
                 else
                   request[:recipient]
                 end
-    
+
     # return lots of information
     return {
       decision: decision,
