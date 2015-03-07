ce87f2aa8468076fd51d287a63f3fa4b8be4e74a
Chris Burnett <dameramu@gmail.com>
2015-03-07 17:10:34 +0000

    Modified method signiatures - we'll have separate methods for individual and group trust/risk assessment

--- traac.rb
+++ traac.rb
@@ -21,14 +21,14 @@ class TraacSTOnly < Raac
   # this is the only thing that changes from the Raac model - we
   # compute risk if we are computing the risk of a request, it means
   # it's been requested...  add it to the history
-  def compute_risk(request, ind_policy, grp_policy)
+  def compute_risk(request, ind_policy)
     # standard risk formulation
     # get the risk and multiply by 1-trust
     loss = Parameters::SENSITIVITY_TO_LOSS[request[:sensitivity]]
     trust = compute_trust(request, ind_policy, grp_policy)
     (1-trust)*loss
   end
-  def compute_trust(request, ind_policy, grp_policy)
+  def compute_trust(request, ind_policy)
     # get the trust rating for this requester
     tm = @trust_models[request[:owner]]
     # evaluate and add result to the trust model
@@ -42,7 +42,7 @@ class TraacSTOnly < Raac
   # the evaluation function
   # positive if shared into read/share/undefined_good
   # negative if shared into deny/undefined_bad (else?)
-  def evaluate_request(request, ind_policy, grp_policy)
+  def evaluate_request(request, ind_policy)
     if [:undefined_good, :read, :share].include? ind_policy[request[:recipient].id]
       true
     elsif [:undefined_bad, :deny].include? ind_policy[request[:recipient].id]
